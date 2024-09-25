f4b32f2559c7d6ba585d0b0bb8410ca60e681545
Colin McLear <mclear@fastmail.com>
2024-09-25 11:32:59 -0400

    Revise button

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { MonsterSearchDialog } from './dialog.js';
 
 Hooks.on('renderActorDirectory', (app, html) => {
   if (html.find('.open5e-button').length === 0) {
-    const button = $('<button class="open5e-button" style="margin: 5px;">Search Monsters</button>');
+    const button = $('<button class="open5e-button" style="margin: 5px;">Fetch Open5e Monsters</button>');
 
     // Attach the dialog trigger
     button.on('click', () => new MonsterSearchDialog());
